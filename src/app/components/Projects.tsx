f46db961db12dd3663c6404dbdb8cfd6950881bd
pranav <pranavbavaskar1@gmail.com>
2025-08-05 21:52:23 +0530

    responsiveness

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -22,14 +22,14 @@ const Projects = () => {
         Stuff I built
       </motion.h3>
 
-      <div className="flex overflow-x-auto flex-nowrap gap-4 mt-4 scrollbar-hide">
+      <div className="flex flex-row overflow-x-auto flex-nowrap gap-4 mt-4 scrollbar-hide">
         {Project.map((item, index) => (
           <motion.div
             initial={{ opacity: 0, y: 40, filter: "blur(8px)" }}
             animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
             transition={{ duration: 0.8, ease: "easeOut" }}
             key={index}
-            className="backdrop-blur bg-black/20 shadow-lg rounded h-[300px] w-[31%] shrink-0 overflow-hidden cursor-pointer flex flex-col space-y-6"
+            className="backdrop-blur bg-black/20 shadow-lg rounded h-[200px] w-[50%] md:h-[300px] md:w-[31%] shrink-0 overflow-hidden cursor-pointer flex flex-col space-y-6"
           >
             <a href={item.link} target="_blank" className="h-[100%]">
               {/* Image */}
@@ -43,7 +43,7 @@ const Projects = () => {
               </div>
               <div className="px-3 mt-4">
                 <div>{item.title}</div>
-                <div className="text-neutral-500 text-sm mt-2 break-words">
+                <div className="hidden md:block text-neutral-500 text-sm mt-2 break-words">
                   {item.desc}
                 </div>
               </div>
